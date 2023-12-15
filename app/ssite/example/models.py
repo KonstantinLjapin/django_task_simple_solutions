f6de474eb8e00f8756cb9ac91ea14ab29e454ed9
KonstantinLjapin <lajpinkonstantin@gmail.com>
2023-12-15 13:54:44 +0300

    model, some element

--- app/ssite/example/models.py
+++ app/ssite/example/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
--- app/ssite/example/views.py
+++ app/ssite/example/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from models import Item
 
-# Create your views here.
+list_items: list = [i for i in range(0, 10)]
+
+
+def index(request):
+    return HttpResponse(f"Hello, world.{list_items}"
+                        f"\n You're at the polls index.")
+
