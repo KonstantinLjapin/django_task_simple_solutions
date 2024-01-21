5cf3ba61a4337d4403efe3b57d7d500e7796f75b
KonstantinLjapin <lajpinkonstantin@gmail.com>
2024-01-21 08:16:14 +0300

    model, some element

--- app/ssite/example/views.py
+++ app/ssite/example/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from models import Item
+from .models import Item
 
 list_items: list = [i for i in range(0, 10)]
 
@@ -8,4 +8,3 @@ list_items: list = [i for i in range(0, 10)]
 def index(request):
     return HttpResponse(f"Hello, world.{list_items}"
                         f"\n You're at the polls index.")
-
